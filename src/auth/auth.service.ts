e3aae5579562d035d784e2510230bc876439632d
dmiante <dmi4ndev@gmail.com>
2023-12-27 00:05:58 -0300

    Refactor auth module for improved user handling and security

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,98 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException
+} from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateAuthDto } from './dto/update-auth.dto'
+import { InjectModel } from '@nestjs/mongoose'
+import { User } from './entities/user.entity'
+import { Model } from 'mongoose'
+import * as bcrypt from 'bcryptjs'
+import { LoginDto } from './dto/login.dto'
+import { JwtService } from '@nestjs/jwt'
+import { JwtPayload } from './interfaces/jwt-payload'
+import { LoginResponse } from './interfaces/login-response'
+import { RegisterDto } from './dto/register.dto'
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      // encrypt pass
+      const { password, ...userData } = createUserDto
+      const newUser = new this.userModel({
+        password: bcrypt.hashSync(password, 10),
+        ...userData
+      })
+      await newUser.save()
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...user } = newUser.toJSON()
+
+      return user
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`)
+      }
+      throw new InternalServerErrorException('Something bad happen!')
+    }
+  }
+
+  async register(registerDto: RegisterDto): Promise<LoginResponse> {
+    return {
+      user: user,
+      token: 'asdasf'
+    }
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
+    const { email, password } = loginDto
+
+    const user = await this.userModel.findOne({ email })
+
+    if (!user) {
+      throw new UnauthorizedException('Not valid credentials - email')
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credentials - password')
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...rest } = user.toJSON()
+
+    return {
+      user: rest,
+      token: this.getJwtToken({ id: user.id })
+    }
   }
 
   findAll() {
-    return `This action returns all auth`;
+    return `This action returns all auth`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} auth`;
+    return `This action returns a #${id} auth`
   }
 
   update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+    console.log(updateAuthDto)
+    return `This action updates a #${id} auth`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return `This action removes a #${id} auth`
+  }
+
+  getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload)
+    return token
   }
 }
